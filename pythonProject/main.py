1f964833179fc18161dd60709893cd22404572a0
P1855 <98693127+P1855@users.noreply.github.com>
2024-01-27 23:19:28 +0530

    Add files via upload

---
+++ pythonProject/main.py
@@ -0,0 +1,30 @@
+import os
+
+import cv2
+from pyzbar.pyzbar import decode
+import matplotlib.pyplot as plt
+import numpy as np
+
+input_dir = "P:/python for ai, dev/pythonProject/Data"
+for j in sorted(os.listdir(input_dir))[:1]:
+    img = cv2.imread(os.path.join(input_dir, j))
+
+    qr_info = decode(img)
+
+    for qr in qr_info:
+        data = qr.data
+        rect = qr.rect
+        polygon = qr.polygon
+
+        print(data)
+        print(rect)
+        print(polygon)
+
+        img = cv2.rectangle(img, (rect.left, rect.top), (rect.left + rect.width, rect.top + rect.height),
+                            (0, 255, 0), 5)
+
+        img = cv2.polylines(img, [np.array(polygon)], True, (255, 0, 0), 5)
+
+        plt.imshow(cv2.cvtColor(img, cv2.COLOR_BGR2RGB))
+        plt.show()
+
